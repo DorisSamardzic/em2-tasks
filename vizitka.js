6112f1fb34f20ccdbf5caca6f7b554c9b31200ee
DorisSamardzic <47415700+DorisSamardzic@users.noreply.github.com>
2019-03-04 13:51:03 +0100

    vizitka css and js update

--- vizitka.js
+++ vizitka.js
@@ -1,25 +1,27 @@
-document.querySelector("#frontSide").addEventListener("click", function(event){
+document.querySelector("#frontSide").addEventListener("click", function (event) {
     event.preventDefault();
     document.querySelector("#backSide").className = "flip flip-frontSide";
-    document.querySelector("#frontSide").className = "flip flip-backSide";  
+    document.querySelector("#frontSide").className = "flip flip-backSide";
 },
-false);
+    false);
 
-document.querySelector("#backSide").addEventListener("click", function(event) {
-event.preventDefault();
-document.getElementById("backSide").className = "flip";
-document.getElementById("frontSide").className = "flip";
+
+
+document.querySelector("#backSide").addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("backSide").className = "flip";
+    document.getElementById("frontSide").className = "flip";
 },
-false);
+    false);
 
-document.querySelector("#facebook").addEventListener("click", function(){
+document.querySelector("#facebook").addEventListener("click", function () {
     window.location.href = "https://www.facebook.com/doris.samardzic";
 })
 
-document.querySelector("#instagram").addEventListener("click", function(){
+document.querySelector("#instagram").addEventListener("click", function () {
     window.location.href = "https://www.instagram.com/doris_samardzic/?hl=hr";
 })
 
-document.querySelector("#linkedin").addEventListener("click", function(){
+document.querySelector("#linkedin").addEventListener("click", function () {
     window.location.href = "https://www.linkedin.com/in/doris-samard%C5%BEi%C4%87-540120181/";
 })
